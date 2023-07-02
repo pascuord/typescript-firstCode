49d7ecf212ea0b2e86dab635b69898a5847aa348
pascuord <pascuord@gmail.com>
2023-07-02 21:31:46 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Crea una función que imprima un mensaje en la consola
+function imprimirMensaje() {
+    const mensaje = '¡Hola, mundo!';
+    console.log(mensaje);
+}
+// Llama a la función para imprimir el mensaje
+imprimirMensaje();
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Crea una función que imprima un mensaje en la consola
+function imprimirMensaje() {
+  const mensaje: string = '¡Hola, mundo!';
+  console.log(mensaje);
+}
+
+// Llama a la función para imprimir el mensaje
+imprimirMensaje();
+
